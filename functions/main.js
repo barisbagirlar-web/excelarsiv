5674c871735229d29210d0f10ba97097a085f917
barisbagirlar-web <barisbagirlar@gmail.com>
2026-08-07 14:35:59 +0300

    feat(commerce): expose secure purchase recovery endpoint

--- functions/main.js
+++ functions/main.js
@@ -2,11 +2,13 @@
 
 const core = require('./index');
 const { createCheckout } = require('./safe-checkout');
+const { recoverPurchase } = require('./recover');
 
 module.exports = {
   createCheckout,
   checkoutStatus: core.checkoutStatus,
   verifyShopierOrder: core.verifyShopierOrder,
+  recoverPurchase,
   createDownloadToken: core.createDownloadToken,
   downloadFile: core.downloadFile,
 };
